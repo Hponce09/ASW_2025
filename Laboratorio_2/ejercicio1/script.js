d5c353f7a706d12d7d6a9a0cb9cae4633a828147
harold ponce <harold.andres.ponce@gmail.com>
2025-10-09 19:04:05 +0200

    final changes

--- Laboratorio_2/ejercicio1/script.js
+++ Laboratorio_2/ejercicio1/script.js
@@ -31,7 +31,7 @@
     son movidas al inicio de su ámbito (scope) antes de que el código se ejecute
 
     ¿Qué diferencias de alcance existen entre var, let y const?
-    var tiene un ambito blobal y pueden ser redeclaradas y modificadas, mientras que 
+    var tiene un ambito global y pueden ser redeclaradas y modificadas, mientras que 
     let y const tiene un ambito de bloque, let pude ser modificada pero no redeclarada
     mientras que const no puede ser modificada ni redeclarada
 
