28d99eb9b1441fa5bb6f366d110bef11d358153f
harold ponce <harold.andres.ponce@gmail.com>
2026-02-05 16:24:32 +0100

    path fix

--- Laboratorio_11/script.js
+++ Laboratorio_11/script.js
@@ -2,13 +2,6 @@ function addElement(){
     const newDiv = document.createElement('div')
     newDiv.className = ('tablero_principal')
     
-    newDiv.style.display = "grid"
-    newDiv.style.gridTemplateColumns = "1fr 1fr 1fr"
-    newDiv.style.gridTemplateRows= "1fr 1fr 1fr"
-    newDiv.style.gap = "2px"
-    newDiv.style.backgroundColor = "red";
-    newDiv.style.width = "300px";
-    newDiv.style.height = "300px";
 
     for(let i = 1 ; i < 10 ; i++ ){
 
