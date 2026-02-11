3d728cb8c4b390e8a9a87a41a05ba659a010496f
harold ponce <harold.andres.ponce@gmail.com>
2026-02-11 14:59:53 +0100

    feat:resetear juego y cambio de color

--- Laboratorio_11/script.js
+++ Laboratorio_11/script.js
@@ -1,4 +1,8 @@
 const currentTurn = document.getElementById("turn");
+const startWidth = window.innerWidth;
+
+let playerWin=false;
+let teclaReinicio= document.getElementById("reinicio")
 
 // Array combinaciones ganadoras
 const combinacionesGanadoras = [
@@ -35,6 +39,9 @@ function startGame() {
 }
 
 function manejarClick(evento) {
+  if(playerWin === true){
+    return;
+  }
   const celda = evento.target;
   const index = celda.dataset.index;
 
@@ -45,12 +52,16 @@ function manejarClick(evento) {
     celda.classList.add("ocupada");
     celda.classList.add(`player${turnoActual}`);
 
+    validacionGanador();
+
     turnoActual = turnoActual === "X" ? "O" : "X";
     currentTurn.textContent = `Turno actual: ${turnoActual}`;
 
     console.log("Estado actual de la partida:", estadoPartida);
-    validacionGanador();
+
   }
+
+  
 }
 
 function validacionGanador(){
@@ -65,8 +76,9 @@ function validacionGanador(){
         pos2 = estadoPartida[combinacion[1]];
         pos3 = estadoPartida[combinacion[2]];
 
-        if (pos1 != "" && pos1 === pos2 && pos2 === pos3){    
-            return alert("Win!!! ", turnoActual);
+        if(pos1 != "" && pos1 === pos2 && pos2 === pos3){   
+          playerWin = true;
+          alert(`Win!!! ${turnoActual}`);
         }
            
     }
@@ -75,4 +87,55 @@ function validacionGanador(){
         alert("empate")
     }
 }
+
+function reinicio(e){
+  var tecla = "r";
+  var key = e.key;
+  if(key.toLowerCase() !== tecla){
+    console.log('hola');
+  }else{
+    console.log(key);
+    
+    //Paso 1: Reiniciar variables (Memoria)
+
+    playerWin=false;
+    estadoPartida = ["", "", "", "", "", "", "", "", ""];
+    turnoActual = "X";
+
+    //Paso 2: Limpiar la "Pizarra" (El DOM)
+
+    var casillas = document.querySelectorAll(".celda") 
+
+    casillas.forEach(casilla=>{
+      casilla.innerHTML="";
+
+      casilla.classList.remove("ocupada");
+      casilla.classList.remove("playerX");
+      casilla.classList.remove("playerO");
+
+    })
+    
+  }
+
+}
+
+window.addEventListener('resize',(e)=>{
+  const actualWidth = window.innerWidth;
+
+  if(startWidth !== actualWidth){
+    document.body.classList.add("visual-dinamic");
+    //console.log(actualWidth + " diferente " + startWidth);
+    
+    // Calcular el color
+    const nuevoTono = actualWidth % 360;
+
+    // Inyectar el color
+    document.body.style.setProperty('--tono', nuevoTono);
+
+    //console.log("Color actual (tono): " + nuevoTono);
+  }
+
+})
+
+teclaReinicio.addEventListener('keydown',reinicio)
 startGame();
